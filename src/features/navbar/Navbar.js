6498d189ed4c647a02f5adb5d0b991c459c3b785
Vibha-Yadav <yadavvibha0508@gmail.com>
2024-05-27 22:24:53 +0530

    added pagination

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -9,7 +9,7 @@ import {
   MenuItems,
   Transition,
 } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, ShoppingCartIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const user = {
   name: 'Tom Cook',
@@ -20,9 +20,7 @@ const user = {
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
   { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  
 ]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
@@ -78,10 +76,15 @@ function NavBar({children}) {
                         type="button"
                         className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                       >
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                        <span className="absolute -inset-1.5" />                        
+                        <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" /> 
+                                              
                       </button>
+                      <span className="inline-flex items-center rounded-md mb-10 -ml-2 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                      3
+                    </span> 
+                      
+                      
 
                       {/* Profile dropdown */}
                       <Menu as="div" className="relative ml-3">
@@ -167,9 +170,13 @@ function NavBar({children}) {
                       className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                     >
                       <span className="absolute -inset-1.5" />
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                      
+                      <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
+                      
                     </button>
+                    <span className="inline-flex items-center rounded-md mb-10 -ml-2 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                      3
+                    </span>
                   </div>
                   <div className="mt-3 space-y-1 px-2">
                     {userNavigation.map((item) => (
@@ -191,7 +198,7 @@ function NavBar({children}) {
 
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Shopify</h1>
           </div>
         </header>
         <main>
