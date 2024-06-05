ba8cdc37688eb113a5d31ba77a63888dea939346
Vibha-Yadav <yadavvibha0508@gmail.com>
2024-06-05 20:09:00 +0530

    created different funtions in productlist for better handling of code

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -147,13 +147,112 @@ export default function ProductList() {
   },[dispatch])
 
   return (
-<div>
-  <div>
-
-  
-    <div className="bg-white">
+  <div className="bg-white">
     <div>
       {/* Mobile filter dialog */}
+      <MobileFilter mobileFiltersOpen={mobileFiltersOpen} setMobileFiltersOpen={setMobileFiltersOpen} handleFilter={handleFilter}></MobileFilter>
+      
+
+      <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900">All Products</h1>
+
+          <div className="flex items-center">
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <MenuButton className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+                  Sort
+                  <ChevronDownIcon
+                    className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                    aria-hidden="true"
+                  />
+                </MenuButton>
+              </div>
+
+              <Transition
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <MenuItems className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="py-1">
+                    {sortOptions.map((option) => (
+                      <MenuItem key={option.name}>
+                        {({ focus }) => (
+                          <p
+                            // href={option.href}
+                            onClick={e=>handleSort(e,option)}
+                            className={classNames(
+                              option.current ? 'font-medium text-gray-900' : 'text-gray-500',
+                              focus ? 'bg-gray-100' : '',
+                              'block px-4 py-2 text-sm'
+                            )}
+                          >
+                            {option.name}
+                          </p>
+                        )}
+                      </MenuItem>
+                    ))}
+                  </div>
+                </MenuItems>
+              </Transition>
+            </Menu>
+
+            <button type="button" className="-m-2 ml-5 p-2 text-gray-400 hover:text-gray-500 sm:ml-7">
+              <span className="sr-only">View grid</span>
+              <Squares2X2Icon className="h-5 w-5" aria-hidden="true" />
+            </button>
+            <button
+              type="button"
+              className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
+              onClick={() => setMobileFiltersOpen(true)}
+            >
+              <span className="sr-only">Filters</span>
+              <FunnelIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+
+        <section aria-labelledby="products-heading" className="pb-24 pt-6">
+          <h2 id="products-heading" className="sr-only">
+            Products
+          </h2>
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+            {/* Filters */}
+            <DesktopFilter handleFilter={handleFilter}></DesktopFilter>
+
+            {/* Product grid */}
+            <div className="lg:col-span-3">
+            {/* product list page */}
+            <ProductGrid products={products}></ProductGrid>
+
+            </div>
+          </div>
+        </section>
+        
+      
+
+      {/* end of products and filters */}
+
+      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+      <Pagination></Pagination>
+    </div>
+    
+        
+  </main>
+    </div>
+</div>
+  );
+}
+
+function MobileFilter({mobileFiltersOpen, setMobileFiltersOpen,handleFilter}){
+  
+  return(
+    
       <Transition show={mobileFiltersOpen}>
         <Dialog className="relative z-40 lg:hidden" onClose={setMobileFiltersOpen}>
           <TransitionChild
@@ -242,78 +341,13 @@ export default function ProductList() {
           </div>
         </Dialog>
       </Transition>
+    
+  )
+}
 
-      <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900">All Products</h1>
-
-          <div className="flex items-center">
-            <Menu as="div" className="relative inline-block text-left">
-              <div>
-                <MenuButton className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-                  Sort
-                  <ChevronDownIcon
-                    className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                    aria-hidden="true"
-                  />
-                </MenuButton>
-              </div>
-
-              <Transition
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <MenuItems className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <div className="py-1">
-                    {sortOptions.map((option) => (
-                      <MenuItem key={option.name}>
-                        {({ focus }) => (
-                          <p
-                            // href={option.href}
-                            onClick={e=>handleSort(e,option)}
-                            className={classNames(
-                              option.current ? 'font-medium text-gray-900' : 'text-gray-500',
-                              focus ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            {option.name}
-                          </p>
-                        )}
-                      </MenuItem>
-                    ))}
-                  </div>
-                </MenuItems>
-              </Transition>
-            </Menu>
-
-            <button type="button" className="-m-2 ml-5 p-2 text-gray-400 hover:text-gray-500 sm:ml-7">
-              <span className="sr-only">View grid</span>
-              <Squares2X2Icon className="h-5 w-5" aria-hidden="true" />
-            </button>
-            <button
-              type="button"
-              className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
-              onClick={() => setMobileFiltersOpen(true)}
-            >
-              <span className="sr-only">Filters</span>
-              <FunnelIcon className="h-5 w-5" aria-hidden="true" />
-            </button>
-          </div>
-        </div>
-
-        <section aria-labelledby="products-heading" className="pb-24 pt-6">
-          <h2 id="products-heading" className="sr-only">
-            Products
-          </h2>
-
-          <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
-            {/* Filters */}
-            <form className="hidden lg:block">
+function DesktopFilter({handleFilter}){
+  return(
+    <form className="hidden lg:block">
               
 
               {filters.map((section) => (
@@ -360,61 +394,12 @@ export default function ProductList() {
                 </Disclosure>
               ))}
             </form>
+  );
+}
 
-            {/* Product grid */}
-            <div className="lg:col-span-3">
-            {/* product list page */}
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
-        
-
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-          {products.map((product) => (
-            <Link to="/product-detail">
-            <div key={product.id} className="group relative border-solid border-2 p-2 border-gray-200">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img
-                  src={product.thumbnail}
-                  alt={product.title}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <div href={product.thumbnail}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.title}
-                    </div>
-                  </h3>
-                  
-                  <p className="mt-1 text-sm text-gray-500">
-                  <StarIcon className="h-5 w-5 inline text-yellow-400" aria-hidden="true" />
-                    <span className="align-bottom">{product.rating}</span></p>
-                </div>
-                <div>
-                
-                <p className="text-sm font-medium text-gray-900">${Math.round(product.price*(1-product.discountPercentage/100))}</p>
-                <p className="text-sm line-through font-medium text-gray-400">${product.price}</p>
-                </div>
-                
-              </div>
-              
-            </div>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </div>
-
-            </div>
-          </div>
-        </section>
-      </main>
-
-      {/* end of products and filters */}
-
-      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+function Pagination(){
+  return(
+    <div>
       <div className="flex flex-1 justify-between sm:hidden">
         <a
           href="#"
@@ -477,10 +462,54 @@ export default function ProductList() {
         </div>
       </div>
     </div>
-    </div>
-  </div>    
-    
-    </div>
-</div>
-  );
+  )
 }
+
+function ProductGrid({products}){
+  return(
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
+        
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+          {products.map((product) => (
+            <Link to="/product-detail">
+            <div key={product.id} className="group relative border-solid border-2 p-2 border-gray-200">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src={product.thumbnail}
+                  alt={product.title}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <div href={product.thumbnail}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.title}
+                    </div>
+                  </h3>
+                  
+                  <p className="mt-1 text-sm text-gray-500">
+                  <StarIcon className="h-5 w-5 inline text-yellow-400" aria-hidden="true" />
+                    <span className="align-bottom">{product.rating}</span></p>
+                </div>
+                <div>
+                
+                <p className="text-sm font-medium text-gray-900">${Math.round(product.price*(1-product.discountPercentage/100))}</p>
+                <p className="text-sm line-through font-medium text-gray-400">${product.price}</p>
+                </div>
+                
+              </div>
+              
+            </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
