c4e25f5e0acf9f2c19961909bdc514abd3190de9
Vibha-Yadav <yadavvibha0508@gmail.com>
2024-06-06 13:44:27 +0530

    modified pagination in productList

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -16,7 +16,11 @@ export function fetchAllProducts() {
   }
   );
 }
-export function fetchProductsByFilters(filter,sort) {
+export function fetchProductsByFilters(filter,sort,pagination) {
+  //filter={"category":["smartphone","laptop"]}
+  //sort={_sort:"price",_order:"desc"}
+  //pagination={_page:1,_limit:10}
+  //TODO: on server we will support multi values in filter
   let queryString = '';
   for (let key in filter) {
     const categoryValues = filter[key];
@@ -29,10 +33,15 @@ export function fetchProductsByFilters(filter,sort) {
 
   for (let key in sort) {
     
-      queryString += `${key}=${sort[key]}}&`;
-    
+      queryString += `${key}=${sort[key]}}&`;    
     
   }
+
+  for (let key in pagination) {
+    
+    queryString += `${key}=${sort[key]}}&`;    
+  
+}
 
 
 
